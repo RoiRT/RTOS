81251e91c204e5502091037c1cadb2bab45a281d
RoiRT <roirt2809@gmail.com>
2025-12-13 15:53:45 +0100

    PWMs

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,20 +1,20 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2025 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2025 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -40,6 +40,9 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htim2;
+TIM_HandleTypeDef htim3;
+
 UART_HandleTypeDef huart2;
 
 /* Definitions for defaultTask */
@@ -53,6 +56,13 @@ const osThreadAttr_t defaultTask_attributes = {
 osThreadId_t TestTask01Handle;
 const osThreadAttr_t TestTask01_attributes = {
   .name = "TestTask01",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityLow,
+};
+/* Definitions for PWM */
+osThreadId_t PWMHandle;
+const osThreadAttr_t PWM_attributes = {
+  .name = "PWM",
   .stack_size = 128 * 4,
   .priority = (osPriority_t) osPriorityLow,
 };
@@ -75,11 +85,10 @@ char uart_buffer[20];
 uint8_t uart_index = 0;
 
 typedef struct {
-    uint8_t modo;
-    uint8_t joy_x;
-    uint8_t joy_y;
+	uint8_t modo;
+	uint8_t joy_x;
+	uint8_t joy_y;
 } ControlMsg_t;
-
 
 /* USER CODE END PV */
 
@@ -87,8 +96,11 @@ typedef struct {
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
+static void MX_TIM3_Init(void);
+static void MX_TIM2_Init(void);
 void StartDefaultTask(void *argument);
 void TestTask(void *argument);
+void PWMTask(void *argument);
 
 /* USER CODE BEGIN PFP */
 
@@ -128,9 +140,11 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART2_UART_Init();
+  MX_TIM3_Init();
+  MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
   HAL_UART_Receive_IT(&huart2, &rx_byte, 1);
-
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
   /* USER CODE END 2 */
 
   /* Init scheduler */
@@ -140,15 +154,15 @@ int main(void)
   controlMutexHandle = osMutexNew(&controlMutex_attributes);
 
   /* USER CODE BEGIN RTOS_MUTEX */
-  /* add mutexes, ... */
+	/* add mutexes, ... */
   /* USER CODE END RTOS_MUTEX */
 
   /* USER CODE BEGIN RTOS_SEMAPHORES */
-  /* add semaphores, ... */
+	/* add semaphores, ... */
   /* USER CODE END RTOS_SEMAPHORES */
 
   /* USER CODE BEGIN RTOS_TIMERS */
-  /* start timers, add new ones, ... */
+	/* start timers, add new ones, ... */
   /* USER CODE END RTOS_TIMERS */
 
   /* Create the queue(s) */
@@ -156,7 +170,7 @@ int main(void)
   myQueue01Handle = osMessageQueueNew (32, sizeof(char), &myQueue01_attributes);
 
   /* USER CODE BEGIN RTOS_QUEUES */
-  /* add queues, ... */
+	/* add queues, ... */
   /* USER CODE END RTOS_QUEUES */
 
   /* Create the thread(s) */
@@ -166,12 +180,15 @@ int main(void)
   /* creation of TestTask01 */
   TestTask01Handle = osThreadNew(TestTask, NULL, &TestTask01_attributes);
 
+  /* creation of PWM */
+  PWMHandle = osThreadNew(PWMTask, NULL, &PWM_attributes);
+
   /* USER CODE BEGIN RTOS_THREADS */
-  /* add threads, ... */
+	/* add threads, ... */
   /* USER CODE END RTOS_THREADS */
 
   /* USER CODE BEGIN RTOS_EVENTS */
-  /* add events, ... */
+	/* add events, ... */
   /* USER CODE END RTOS_EVENTS */
 
   /* Start scheduler */
@@ -180,12 +197,11 @@ int main(void)
   /* We should never get here as control is now taken by the scheduler */
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
-  {
+	while (1) {
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-  }
+	}
   /* USER CODE END 3 */
 }
 
@@ -231,6 +247,124 @@ void SystemClock_Config(void)
 }
 
 /**
+  * @brief TIM2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM2_Init(void)
+{
+
+  /* USER CODE BEGIN TIM2_Init 0 */
+
+  /* USER CODE END TIM2_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+
+  /* USER CODE BEGIN TIM2_Init 1 */
+
+  /* USER CODE END TIM2_Init 1 */
+  htim2.Instance = TIM2;
+  htim2.Init.Prescaler = 7;
+  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim2.Init.Period = 999;
+  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
+  if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  if (HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM2_Init 2 */
+
+  /* USER CODE END TIM2_Init 2 */
+  HAL_TIM_MspPostInit(&htim2);
+
+}
+
+/**
+  * @brief TIM3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM3_Init(void)
+{
+
+  /* USER CODE BEGIN TIM3_Init 0 */
+
+  /* USER CODE END TIM3_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+
+  /* USER CODE BEGIN TIM3_Init 1 */
+
+  /* USER CODE END TIM3_Init 1 */
+  htim3.Instance = TIM3;
+  htim3.Init.Prescaler = 7;
+  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim3.Init.Period = 999;
+  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
+  if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim3, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM3_Init 2 */
+
+  /* USER CODE END TIM3_Init 2 */
+  HAL_TIM_MspPostInit(&htim3);
+
+}
+
+/**
   * @brief USART2 Initialization Function
   * @param None
   * @retval None
@@ -258,7 +392,7 @@ static void MX_USART2_UART_Init(void)
     Error_Handler();
   }
   /* USER CODE BEGIN USART2_Init 2 */
-  HAL_UART_Receive_IT(&huart2, (uint8_t *)&rx_byte, 1);
+	HAL_UART_Receive_IT(&huart2, (uint8_t*) &rx_byte, 1);
   /* USER CODE END USART2_Init 2 */
 
 }
@@ -273,104 +407,123 @@ static void MX_GPIO_Init(void)
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
 
 }
 
 /* USER CODE BEGIN 4 */
-void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
-{
-    if (huart->Instance == USART2)
-    {
-        char c = rx_byte;
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
+	if (huart->Instance == USART2) {
+		char c = rx_byte;
 
-        // Meter el carácter en la cola SIN BLOQUEO (desde ISR)
-        osMessageQueuePut(myQueue01Handle, &c, 0, 0);
+		// Meter el carácter en la cola SIN BLOQUEO (desde ISR)
+		osMessageQueuePut(myQueue01Handle, &c, 0, 0);
 
-        // Reiniciar recepción
-        HAL_UART_Receive_IT(&huart2, &rx_byte, 1);
-    }
+		// Reiniciar recepción
+		HAL_UART_Receive_IT(&huart2, &rx_byte, 1);
+	}
 }
 
 /* USER CODE END 4 */
 
 /* USER CODE BEGIN Header_StartDefaultTask */
 /**
-  * @brief  Function implementing the defaultTask thread.
-  * @param  argument: Not used
-  * @retval None
-  */
+ * @brief  Function implementing the defaultTask thread.
+ * @param  argument: Not used
+ * @retval None
+ */
 /* USER CODE END Header_StartDefaultTask */
 void StartDefaultTask(void *argument)
 {
   /* USER CODE BEGIN 5 */
-  /* Infinite loop */
-  for(;;)
-  {
-    osDelay(1);
-  }
+	/* Infinite loop */
+	for (;;) {
+		osDelay(1);
+	}
   /* USER CODE END 5 */
 }
 
 /* USER CODE BEGIN Header_TestTask */
 /**
-* @brief Function implementing the TestTask01 thread.
-* @param argument: Not used
-* @retval None
-*/
+ * @brief Function implementing the TestTask01 thread.
+ * @param argument: Not used
+ * @retval None
+ */
 /* USER CODE END Header_TestTask */
 void TestTask(void *argument)
 {
   /* USER CODE BEGIN TestTask */
+	/* Infinite loop */
+	char c;
+	char buffer[20];
+	uint8_t idx = 0;
+
+	for (;;) {
+		// Espera bloqueante: eficiente
+		if (osMessageQueueGet(myQueue01Handle, &c, NULL, osWaitForever)
+				== osOK) {
+			// 1) Modo M / A
+			if (c == 'C') {
+				osMutexAcquire(controlMutexHandle, osWaitForever);
+				modo = 1;
+				osMutexRelease(controlMutexHandle);
+			} else if (c == 'c') {
+				osMutexAcquire(controlMutexHandle, osWaitForever);
+				modo = 0;
+				osMutexRelease(controlMutexHandle);
+			}
+
+			// 2) Joystick Xnnn,Ynnn
+			else {
+				if ((c == '\n') || (c == '\r')) {
+					buffer[idx] = '\0';
+
+					int x, y;
+					if (sscanf(buffer, "X%d,Y%d", &x, &y) == 2) {
+						osMutexAcquire(controlMutexHandle, osWaitForever);
+						joy_x = x;
+						joy_y = y;
+						osMutexRelease(controlMutexHandle);
+					}
+					memset(buffer, 0, sizeof(buffer));
+					idx = 0;
+				} else if (idx < sizeof(buffer) - 1) {
+					buffer[idx++] = c;
+				}
+			}
+		}
+	}
+  /* USER CODE END TestTask */
+}
+
+/* USER CODE BEGIN Header_PWMTask */
+/**
+* @brief Function implementing the PWM thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_PWMTask */
+void PWMTask(void *argument)
+{
+  /* USER CODE BEGIN PWMTask */
   /* Infinite loop */
-    char c;
-    char buffer[20];
-    uint8_t idx = 0;
+    uint32_t duty = 3000;                 // valor inicial
+    uint32_t duty_max = __HAL_TIM_GET_AUTORELOAD(&htim3);   // 3999
+    uint32_t step = 10;                   // incremento por ciclo
 
     for(;;)
     {
-    	// Espera bloqueante: eficiente
-    	if (osMessageQueueGet(myQueue01Handle, &c, NULL, osWaitForever) == osOK)
+        if (duty < duty_max)
         {
-            // 1) Modo M / A
-            if (c == 'C')
-            {
-                osMutexAcquire(controlMutexHandle, osWaitForever);
-                modo = 1;
-                osMutexRelease(controlMutexHandle);
-            }
-            else if (c == 'c')
-            {
-                osMutexAcquire(controlMutexHandle, osWaitForever);
-                modo = 0;
-                osMutexRelease(controlMutexHandle);
-            }
+            duty += step;
+            if (duty >= duty_max) duty = 3000;
 
-            // 2) Joystick Xnnn,Ynnn
-            else
-            {
-                if ((c == '\n') || (c == '\r'))
-                {
-                    buffer[idx] = '\0';
-
-                    int x, y;
-                    if (sscanf(buffer, "X%d,Y%d", &x, &y) == 2)
-                    {
-                        osMutexAcquire(controlMutexHandle, osWaitForever);
-                        joy_x = x;
-                        joy_y = y;
-                        osMutexRelease(controlMutexHandle);
-                    }
-                    memset(buffer, 0, sizeof(buffer));
-                    idx = 0;
-                }
-                else if (idx < sizeof(buffer) - 1)
-                {
-                    buffer[idx++] = c;
-                }
-            }
+            __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, duty);
         }
+
+        osDelay(90);   // velocidad de subida (20 ms por paso)
     }
-  /* USER CODE END TestTask */
+  /* USER CODE END PWMTask */
 }
 
 /**
@@ -380,11 +533,10 @@ void TestTask(void *argument)
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
+	/* User can add his own implementation to report the HAL error return state */
+	__disable_irq();
+	while (1) {
+	}
   /* USER CODE END Error_Handler_Debug */
 }
 
